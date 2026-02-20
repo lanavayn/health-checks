73b9e11846ac3c98ac14fa4e808fcf65d7dc21ee
Lana Vayner <lanazvorygin@gmail.com>
2026-02-20 17:06:56 -0500

    Add call to disk_full function

--- all_checks.py
+++ all_checks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+  
 import os
 import sys
 
@@ -11,5 +11,12 @@ def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
+
+    if disk_full():
+        print("Disk Full.")
+        sys.exit(1)
+
+    print("Everything ok.")
+    sys.exit(0)
 
 main()
